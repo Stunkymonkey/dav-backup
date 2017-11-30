5f5d9b0c2d0a89eaf8fd813c87ee41b1b58d3714
Benedikt Heine <bebe@bebehei.de>
2017-12-01 00:05:58 +0100

    Double quote all variables consistently

--- dav-backup.sh
+++ dav-backup.sh
@@ -4,7 +4,7 @@
 source "${XDG_CONFIG_HOME:-$HOME/.config}/dav-backup/config"
 export WGETRC="${XDG_CONFIG_HOME:-$HOME/.config}/dav-backup/credentials"
 
-if [[ ! -w $OUT/ ]]; then
+if [[ ! -w "$OUT/" ]]; then
 	echo "Error: unable to write into directory '${OUT}'!"
 	exit 1
 fi
@@ -26,36 +26,36 @@ case "$SERVICE" in
 		exit 1
 esac
 
-for addr in $ADDRESSBOOK; do
+for addr in "$ADDRESSBOOK"; do
 	wget -q \
-		-O $OUT/${addr}-$DATE.vcf \
+		-O "$OUT/${addr}-$DATE.vcf" \
 		"$(printf "${CARDURL}" "${addr}")"
 	
-	if [ -s $OUT/$addr-$DATE.vcf ]; then
+	if [ -s "$OUT/$addr-$DATE.vcf" ]; then
 		echo "$addr successfully downloaded"
 	else
 		echo "unknwon error"
-		rm $OUT/$addr-$DATE.vcf
+		rm "$OUT/$addr-$DATE.vcf"
 		exit $?
 	fi
 
-	tar r -C $OUT -f $OUT/DAV-$DATE.tar.gz $addr-$DATE.vcf
-	rm $OUT/$addr-$DATE.vcf
+	tar r -C "$OUT" -f "$OUT/DAV-$DATE.tar.gz" "$addr-$DATE.vcf"
+	rm "$OUT/$addr-$DATE.vcf"
 done
 
-for cal in $CALENDAR; do
+for cal in "$CALENDAR"; do
 	wget -q \
-		-O $OUT/$cal-$DATE.ics \
+		-O "$OUT/$cal-$DATE.ics" \
 		"$(printf "${CALURL}" "${cal}")"
 	
-	if [ -s $OUT/$cal-$DATE.ics ]; then
+	if [ -s "$OUT/$cal-$DATE.ics" ]; then
 		echo "$cal successfully downloaded"
 	else
 		echo "unknwon error"
-		rm $OUT/$cal-$DATE.ics
+		rm "$OUT/$cal-$DATE.ics"
 		exit $?
 	fi
 
-	tar r -C $OUT -f $OUT/DAV-$DATE.tar.gz $cal-$DATE.ics
-	rm $OUT/$cal-$DATE.ics
+	tar r -C "$OUT" -f "$OUT/DAV-$DATE.tar.gz" "$cal-$DATE.ics"
+	rm "$OUT/$cal-$DATE.ics"
 done
