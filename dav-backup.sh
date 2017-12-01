ca49188682ad9e0f6178197fee713b17c10d679e
Benedikt Heine <bebe@bebehei.de>
2017-12-01 01:24:34 +0100

    Unify download function

--- dav-backup.sh
+++ dav-backup.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+die() { echo "$*" >&2; exit 1; }
+
+# download first argument and put with filename of argument two into archive
+download(){
+
+	url="${1?No URL given to download.}"
+	file="${2?No filename given to save.}"
+
+	wget -q \
+		-O "$OUT/${file}" \
+		"${url}"
+
+	if [ -s "$OUT/$file" ]; then
+		echo "SUCCESS: $file"
+	else
+		die "Unknown Error at '${file}'!"
+	fi
+
+	tar r -C "$OUT" -f "$OUT/DAV-$DATE.tar.gz" "${file}"
+	rm "$OUT/${file}"
+}
+
 # getting config
 source "${XDG_CONFIG_HOME:-$HOME/.config}/dav-backup/config"
 export WGETRC="${XDG_CONFIG_HOME:-$HOME/.config}/dav-backup/credentials"
 
 if [[ ! -w "$OUT/" ]]; then
-	echo "Error: unable to write into directory '${OUT}'!"
-	exit 1
+	die "Error: unable to write into directory '${OUT}'!"
 fi
 
 # create empty tarpackage
@@ -22,8 +43,7 @@ case "$SERVICE" in
 		CALURL="$HOST/dav.php/calendars/$DAVUSER/%s?export"
 		;;
 	*)
-		echo "Unknown Service"
-		exit 1
+		die "Unknown Service"
 esac
 
 for addr in "${ADDRESSBOOKS[@]}"; do
@@ -31,20 +51,7 @@ for addr in "${ADDRESSBOOKS[@]}"; do
 	IFS=: read id name <<< "${addr}"
 	[ -n "${name}" ] || name="${id}"
 
-	wget -q \
-		-O "$OUT/${name}-$DATE.vcf" \
-		"$(printf "${CARDURL}" "${id}")"
-	
-	if [ -s "$OUT/$name-$DATE.vcf" ]; then
-		echo "$name successfully downloaded"
-	else
-		echo "unknwon error"
-		rm "$OUT/$name-$DATE.vcf"
-		exit $?
-	fi
-
-	tar r -C "$OUT" -f "$OUT/DAV-$DATE.tar.gz" "$name-$DATE.vcf"
-	rm "$OUT/$name-$DATE.vcf"
+	download "$(printf "${CARDURL}" "${id}")" "${name}-$DATE.vcf"
 done
 
 for cal in "${CALENDARS[@]}"; do
@@ -52,18 +59,5 @@ for cal in "${CALENDARS[@]}"; do
 	IFS=: read id name <<< "${cal}"
 	[ -n "${name}" ] || name="${id}"
 
-	wget -q \
-		-O "$OUT/$name-$DATE.ics" \
-		"$(printf "${CALURL}" "${id}")"
-	
-	if [ -s "$OUT/$name-$DATE.ics" ]; then
-		echo "$name successfully downloaded"
-	else
-		echo "unknwon error"
-		rm "$OUT/$name-$DATE.ics"
-		exit $?
-	fi
-
-	tar r -C "$OUT" -f "$OUT/DAV-$DATE.tar.gz" "$name-$DATE.ics"
-	rm "$OUT/$name-$DATE.ics"
+	download "$(printf "${CALURL}" "${id}")" "$name-$DATE.ics"
 done
